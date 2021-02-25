4b8e369d6193fff315443ec288f996fa09a9881f
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-25 10:04:43 +0000

    container >> 1.0.x-dev

--- src/Container.php
+++ src/Container.php
@@ -5,7 +5,25 @@ declare(strict_types=1);
 namespace Pollen\Container;
 
 use League\Container\Container as BaseContainer;
+use League\Container\ReflectionContainer;
 
 class Container extends BaseContainer
 {
+    /**
+     * Activation de l'auto wring
+     *
+     * @param bool $cached
+     *
+     * @return static
+     */
+    public function enableAutoWiring(bool $cached = false): self
+    {
+        $reflectionContainer = new ReflectionContainer();
+
+        if ($cached === true) {
+            $reflectionContainer->cacheResolutions();
+        }
+
+        return $this->delegate($reflectionContainer);
+    }
 }
