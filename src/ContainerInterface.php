a8eff15ac65a8e869c01ca4e9f117629b4144318
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-26 14:05:47 +0000

    container >> 1.0.x-dev

---
+++ src/ContainerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Container;
+
+use Psr\Container\ContainerInterface as PsrContainer;
+
+interface ContainerInterface extends PsrContainer
+{
+    /**
+     * Activation de l'auto wring
+     *
+     * @param bool $cached
+     *
+     * @return static
+     */
+    public function enableAutoWiring(bool $cached = false): ContainerInterface;
+}
